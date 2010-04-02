0c97230e53c8a8eefde8024ac1f81721ad645992
Jehan Monnier <jehan.monnier@linphone.org>
2010-04-02 11:56:22 +0200

    add android build scripts

---
+++ build/android/Android.mk
@@ -0,0 +1,68 @@
+##
+## Android.mk -Android build script-
+##
+##
+## Copyright (C) 2010  Belledonne Communications, Grenoble, France
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; either version 2 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU Library General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program; if not, write to the Free Software
+##  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+##
+
+
+LOCAL_PATH:= $(call my-dir)/../../
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libortp
+
+
+LOCAL_SRC_FILES := \
+	src/str_utils.c	\
+	src/port.c \
+	src/rtpparse.c \
+	src/rtpsession.c \
+	src/rtpsession_inet.c \
+	src/rtpsession_priv.h \
+	src/jitterctl.c \
+	src/jitterctl.h \
+	src/rtpsignaltable.c  \
+	src/rtptimer.c \
+	src/rtptimer.h \
+	src/posixtimer.c \
+	src/ortp.c \
+	src/scheduler.c \
+	src/scheduler.h \
+	src/avprofile.c \
+	src/sessionset.c \
+	src/telephonyevents.c \
+	src/payloadtype.c \
+	src/rtcp.c \
+	src/utils.c \
+	src/utils.h \
+	src/rtcpparse.c \
+	src/event.c \
+	src/stun.c \
+	src/stun_udp.c \
+	src/srtp.c \
+	src/b64.c 
+
+LOCAL_CFLAGS += \
+	-UHAVE_CONFIG_H \
+	-include $(LOCAL_PATH)/build/android/ortp_AndroidConfig.h
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH) \
+	$(LOCAL_PATH)/include
+
+LOCAL_LDLIBS += -lpthread
+
+include $(BUILD_STATIC_LIBRARY)
