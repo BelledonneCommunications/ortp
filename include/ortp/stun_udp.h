5deb3282633f97aea1e4cc90467e83f88ca279cf
Jehan Monnier <jehan.monnier@linphone.org>
2010-12-17 14:17:16 +0100

    fix flexisip compilation issue

--- include/ortp/stun_udp.h
+++ include/ortp/stun_udp.h
@@ -94,8 +94,12 @@ typedef LONGLONG Int64;
 #else
 
 typedef int Socket;
+#ifndef INVALID_SOCKET 
 #define INVALID_SOCKET -1
+#endif
+#ifndef SOCKET_ERROR 
 #define SOCKET_ERROR -1
+#endif
 
 #define closesocket(fd) close(fd)
 
