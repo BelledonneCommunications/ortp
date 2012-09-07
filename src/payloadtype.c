cf067f6e6f7509cd2576a4ac4e89b47ab9296495
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2012-09-07 17:01:28 +0200

    Specific header inclusions.

--- src/payloadtype.c
+++ src/payloadtype.c
@@ -17,7 +17,9 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include "ortp/ortp.h"
+#include "ortp/logging.h"
+#include "ortp/payloadtype.h"
+#include "ortp/str_utils.h"
 
 
 char *payload_type_get_rtpmap(PayloadType *pt)
