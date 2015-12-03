15c79d12ab280a3a017961d425a6db79edfbf3aa
Sandrine Avakian <sandrine.avakian@belledonne-communications.com>
2015-12-03 13:22:54 +0100

    Added the possibility in network simulator to randomly lose only RTP packets (instead of RTP and RTCP).

--- src/netsim.c
+++ src/netsim.c
@@ -182,6 +182,7 @@ void rtp_session_enable_network_simulation(RtpSession *session, const OrtpNetwor
     			);
 	}else{
 		session->net_sim_ctx=NULL;
+        ortp_message("rtp_session_enable_network_simulation:DISABLING NETWORK SIMULATION");
 		if (sim!=NULL) ortp_network_simulator_destroy(sim);
 	}
 }
@@ -342,7 +343,7 @@ mblk_t * rtp_session_network_simulate(RtpSession *session, mblk_t *input, bool_t
 
 	om=input;
 
-	/*while packet is stored in network simulator queue, keep its type in reserved1 space*/
+    /*while packet is stored in network simulator queue, keep its type in reserved1 space*/
 	if (om != NULL){
 		sim->total_count++;
 		om->reserved1 = *is_rtp_packet;
@@ -355,7 +356,8 @@ mblk_t * rtp_session_network_simulate(RtpSession *session, mblk_t *input, bool_t
 	if (sim->params.max_bandwidth>0){
 		om=simulate_bandwidth_limit_and_jitter(session,om);
 	}
-	if (sim->params.loss_rate>0 && om){
+  
+	if (sim->params.loss_rate>0 && om && (*is_rtp_packet == TRUE) && (sim->params.RTP_only==TRUE) ){
 		om=simulate_loss_rate(sim,om);
 	}
 	/*finally when releasing the packet from the simulator, reset the reserved1 space to default,
