c8d1af1e2f7d59ab8f9c397b1c145127af9033f5
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2018-02-05 17:39:39 +0100

    Initialize CMAKE_BUILD_TYPE in RPM spec file.

--- build/rpm/ortp.spec.cmake
+++ build/rpm/ortp.spec.cmake
@@ -80,7 +80,7 @@ develop programs using the oRTP library.
 %setup -n %{name}-%{version}-%build_number
 
 %build
-%{expand:%%%cmake_name} . -DCMAKE_INSTALL_LIBDIR=%{_lib} -DCMAKE_PREFIX_PATH:PATH=%{_prefix} @RPM_ALL_CMAKE_OPTIONS@
+%{expand:%%%cmake_name} . -DCMAKE_BUILD_TYPE=@CMAKE_BUILD_TYPE@ -DCMAKE_INSTALL_LIBDIR=%{_lib} -DCMAKE_PREFIX_PATH:PATH=%{_prefix} @RPM_ALL_CMAKE_OPTIONS@
 make %{?_smp_mflags}
 
 %install
